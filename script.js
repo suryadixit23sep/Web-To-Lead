0ad6a9befd0ec31e3f3cafb02c911378bbcc58fe
FZHB8H_gme <surya.nathdixit@gm.com>
2025-04-11 15:50:38 +0530

    added if else to validate recaptcha

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function timestamp() {
+let captchachecked = false;
+
+function timestamp(event) {
   var response = document.getElementById("g-recaptcha-response");
   if (response == null || response.value.trim() == "") {
     var elems = JSON.parse(
@@ -8,6 +10,19 @@ function timestamp() {
     document.getElementsByName("captcha_settings")[0].value =
       JSON.stringify(elems);
   }
+  if(captchachecked){
+
+  }
+  else{
+      alert("please check the reCaptcha box to submit the lead");
+      event.preventDefault();
+  }
 }
 setInterval(timestamp, 500);
 //above function we got it from the generate web to lead form
+
+
+function captchasuccess()
+{
+    captchachecked = true;
+}
