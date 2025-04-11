43298a3dc2d289d32223e0ad1ec86a8d73d43be8
FZHB8H_gme <surya.nathdixit@gm.com>
2025-04-11 15:39:14 +0530

    added Recaptcha Code

---
+++ script.js
@@ -0,0 +1,13 @@
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
+//above function we got it from the generate web to lead form
